6be30a5d2b5b1bfb74001c0d96680f1e756522f9
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-02-13 16:11:15 +0900

    Create main_server_viedo.py

---
+++ NGV project/Server_laptop/main_server_viedo.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import threading
+import time
+import FrameProcess
+# import dataserver
+import sys
+import os
+sys.path.append(os.path.join(os.getcwd(), 'python/'))
+sys.path.append(os.getcwd().replace('darknet', ''))
+# sys.path.append(os.getcwd().replace('darknet', 'camData/'))
+# sys.path.append(os.getcwd().replace('darknet', 'img1/'))
+
+
+class MainThread:
+    def __init__(self):
+        # self.ts = Telecommunication()
+        # self.ts.streaming()
+        # self.ts.streaming.after(1000, FrameProcess.frame2video())
+        time.sleep(3)
+        FrameProcess.frame2video()
+        #FrameProcess.frame2video().after(3000, self.runYolo())
+        # self.runYolo()
+        # self.runYolo().after(3000, self.ts.sendcoordinates())
+        
+    '''
+    def runYolo(self):
+        # command: run yolo
+        os.system(
+            './darknet detector demo data/obj.data cfg/yolov3.cfg backup/yolov3_3300.weights '
+            'data/media/video{},avi'.format(video_name_cnt))
+        print("Now Detecting Black Ice...")
+    '''
+
+MainThread()
