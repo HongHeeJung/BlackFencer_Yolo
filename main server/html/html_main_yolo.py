4dada919b3771a7b07a84d9f05c396ceeb96a609
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-02-20 20:45:20 +0900

    Create html_main_yolo.py

---
+++ main server/html/html_main_yolo.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*- 
+import cv2
+import numpy as np
+import threading
+import io
+import socket
+import sys
+import struct
+import time
+import pickle
+import zlib
+import os
+import shutil
+
+
+# input server socket
+host = "192.168.0.116"
+port = 4000
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((host, port))
+print("Connected")
+
+
+def main():
+    # receive ip camera
+    cap = cv2.VideoCapture('http://192.168.0.116:8081/video?dummy=param.mjpg')
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    time.sleep(2)
+    
+    # socket
+    sc = threading.Thread(target=send_coord, args=(client_socket,))
+    sc.start()
+   
+
+def send_coord(sock):
+    global conn
+    while True:
+        print("waiting....")
+        time.sleep(4)
+        # read yolo_mark bounding box
+        '''
+        with open("/home/heejunghong/BlackfencerWeb/index.html", 'w') as my_file_2:     
+            my_file_2.write('0')
+            print("Initialize the bounding box's coordinate to 0")
+            my_file_2.close()
+        '''    
+        with open("/home/heejunghong/BlackfencerWeb/index.html", 'r') as my_file_2:
+            data = my_file_2.read()
+            print("Read the bounding box's coordinate")
+            #_data = str(data).replace('я', '')
+            _data = data.strip()
+            print("_data " ,_data)
+            
+            #client_socket.sendall((str(len(_data))).ljust(16) + _data)
+            client_socket.sendall(_data)
+            
+            print("Send bounding box's coordinate successfully!")
+            my_file_2.close()
+            time.sleep(2)
+
+
+
+if __name__ == '__main__':
+    main()
